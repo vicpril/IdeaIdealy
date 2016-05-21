30dcf56a3a9c59506f2d009b82de043d1be76a75
Viktor Prilepin <vicpride@gmail.com>
2016-05-21 06:56:59 +0600

    update author.php

--- wp-content/themes/xaver/author.php
+++ wp-content/themes/xaver/author.php
@@ -95,7 +95,7 @@ else
 
     <p>
         <?php
-        query_posts(array('&orderby=date&order=DESC&posts_per_page=-1&meta_key=coauthor&meta_value=' . $curauth->display_name));
+        query_posts('lang=ru&&orderby=date&order=DESC&posts_per_page=-1&meta_key=coauthor&meta_value=' . $curauth->display_name);
     //         if($curauth->user_description<>''): echo "Краткая биография: <br>". str_replace("\n","<br>",$curauth->user_description);
         if ($curauth->user_description <> ''): echo str_replace("\n", "<br>", $curauth->user_description);
 
@@ -125,37 +125,78 @@ else
     
     
     if (have_posts()){ 
-
+        $archive = false;
         ?>
             <h1 class="title">Proceedings of the author</h1>
         <?php
 
         while (have_posts()) : the_post();
 
-
-            if ($post->ID == $do_not_duplicate)
+            $yearno = get_post_meta($post->ID, 'yearno', 'single');
+            
+            if ($yearno >= 2016) {
+                if ($post->ID == $do_not_duplicate)
                 continue;
-            update_post_caches($posts);
+                update_post_caches($posts);
+
+
+                $no = get_post_meta($post->ID, 'no', 'single');
+                $tom = get_post_meta($post->ID, 'tom', 'single');
+
+                $title =    get_field('title_en', $post->ID);
+                if (empty($title)) {$title = $post->post_title; }
+
+                $pid_en = pll_get_post_translations($post->ID);
+
+                $permalink = get_permalink($pid_en['en']);
+
+                ?>
+
+                <div class="info">
+                    <div class="info-list">
+                        <a style='font-size:16px;font-weight:bold;text-decoration:none' href="<?=$permalink?>" rel="bookmark" title="Go to the article: <?php the_title(); ?>"><?=$title?></a><? echo " / <div style='display:inline' class='crumbs'><a href='/en/archive#$yearno' title='Go to the magazine'>Magazine " . $yearno . " year</a> &raquo; <a href='/en/nomer?yearno=$yearno&no=$no&tom=$tom' title='Go to this No.'>№" . $no . ", Volume " . $tom . "</a></div>"; ?>
+                <?php //if(function_exists(wt_the_coauthors_link)): wt_the_coauthors_link("<br><b>","</b>"); endif;
+                ?>
+                        <!--<br>
+                        Аннотация:
+                <?php the_excerpt(); ?> -->
+                    </div>
+                </div>
+            <?php
+            }
+            
+            endwhile;
+            
+            
+            
+            /*
+             * English archive
+             */
+            while (have_posts()) : the_post();
 
             $yearno = get_post_meta($post->ID, 'yearno', 'single');
-            $no = get_post_meta($post->ID, 'no', 'single');
-            $tom = get_post_meta($post->ID, 'tom', 'single');
-            ?>
+            
+            if ($yearno < 2016) {
+                $archive = true;
+            }
+            
+            endwhile;
+            
+            if ($archive) {
+                ?>
+                <!--<h1 class="title">English Archive (Until 2016 year)</h1>-->
+                
+                    <a class="red-link" href="<?php home_url('home')?>/archive-en/author/<?=$curauth->user_nicename?>" title="<?=$curauth->us_name_en?>'s archive (Until 2016 year)">English Archive</a>
+                
+                <?php
+            }
+            
+            
+            
+            
+            
 
-            <div class="info">
-                <div class="info-list">
-                    <a style='font-size:16px;font-weight:bold;text-decoration:none' href="<?php the_permalink() ?>" rel="bookmark" title="Перейти к материалу: <?php the_title(); ?>"><?php the_title() ?></a><? echo " / <div style='display:inline' class='crumbs'><a href='/archive#$yearno' title='Перейти к номерам данного года'>Журнал " . $yearno . " года</a> &raquo; <a href='/nomer?yearno=$yearno&no=$no&tom=$tom' title='Перейти к номеру журнала'>№" . $no . ", Том " . $tom . "</a></div>"; ?>
-            <?php //if(function_exists(wt_the_coauthors_link)): wt_the_coauthors_link("<br><b>","</b>"); endif;
-            ?>
-                    <!--<br>
-                    Аннотация:
-            <?php the_excerpt(); ?> -->
-                </div>
-            </div>
-
-        <?php endwhile; ?>
-
-    <? // }else  ?>
+    ?>
         <!--<p class="error">У данного автора еще нет материалов.</p>-->
     <?php } 
 
